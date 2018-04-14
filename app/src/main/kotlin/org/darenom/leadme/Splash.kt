decbca3c839d340ee7495a7a72ee8ab9cbeb1a05
dma <areno.dm@gmail.com>
2018-04-14 12:34:32 +0200

    splash text

--- app/src/main/kotlin/org/darenom/leadme/Splash.kt
+++ app/src/main/kotlin/org/darenom/leadme/Splash.kt
@@ -1,10 +1,12 @@
 package org.darenom.leadme
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.app.Activity
 import android.os.Handler
 import android.support.annotation.Nullable
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 import org.darenom.leadme.ui.MainActivity
 
 
@@ -18,9 +20,17 @@ class Splash : Activity() {
     override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Handler().postDelayed(Runnable {
+        RunAnimation()
+        Handler().postDelayed({
             startActivity(Intent(this@Splash, MainActivity::class.java))
             finish()
         }, 3000)
+    }
+
+    private fun RunAnimation() {
+        val a = AnimationUtils.loadAnimation(this@Splash, R.anim.scale)
+        a.reset()
+        text.clearAnimation()
+        text.startAnimation(a)
     }
 }
